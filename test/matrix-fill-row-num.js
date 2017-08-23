2f9263e73196ec70eb1f4f6883374db893fa9b71
User <yasser.morteza@gmail.com>
2017-08-23 16:18:08 +0200

    .travis.yml

--- test/matrix-fill-row-num.js
+++ test/matrix-fill-row-num.js
@@ -9,11 +9,13 @@ describe('matrixFillRowNum', function(){
   })
 
   it('produces multi dimensional array', function(){
+
     var result = matrixFillRowNum(matrix);
     expect(result).to.be.an('array');
     expect(result[0]).to.be.an('array');
     expect(result.length).to.be(4);
     expect(result[0].length).to.be(4);
+
     for(i=0; i<result[0].length; i++){
       expect(result[0][i]).to.be.a('number');
       expect(result[0][i]).to.not.be('string');
--- test/matrix-fill-text.js
+++ test/matrix-fill-text.js
@@ -9,14 +9,17 @@ describe('test matrixFillText', function(){
   })
   it('has to produce array', function(){
     var result = matrixFillText(matrix);
+
     expect(result).to.be.an('array');
     expect(result[0]).to.be.an('array');
     expect(result.length).to.be(4);
     expect(result[0].length).to.be(5);
     expect(result[0].indexOf(3)).to.be(-1);
+
     for(i=0; i<result[0].length; i++){
-    expect(result[0][i]).to.not.be('number');
-    expect(result[0][i]).to.be.a('string');
+      expect(result[0][i]).to.not.be('number');
+      expect(result[0][i]).to.be.a('string');
     }
+
   })
 });
